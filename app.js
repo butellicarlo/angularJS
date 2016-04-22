5b5dc325e10eef0fe0e6980ca11f9896ec2946b6
carlitos26 <carlito@Carlos-MacBook-Pro.local>
2016-04-22 20:25:25 +0200

    First commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   var gem = { 
       name: 'Azurite', 
       price: 2.95, 
-      description: 'Starting in RubyGems 2.0, a Specification can hold arbitrary metadata. See metadata for restrictions on the format and size of metadata items you may add to a specification'; 
+      description: 'Starting in RubyGems 2.0, a Specification can hold arbitrary metadata. See metadata for restrictions on the format and size of metadata items you may add to a specification',
       canPurchase: false
   };
   var app = angular.module('gemStore', []);
